983698efc11507efc758524eb7ab504c534897c7
yburkul <yburkul123@gmail.com>
2022-03-27 12:13:17 +0530

    Added:Find max Float number

---
+++ MaximumNum/MaximumNum/MaxNum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximumNum
+{
+    class MaxNum
+    {
+        public int Maximum(int numOne, int numTwo, int numThree)
+        {
+            if (numOne.CompareTo(numTwo) > 0 && numOne.CompareTo(numThree) > 0)
+            {
+                return numOne;
+            }
+            else if (numTwo.CompareTo(numOne) > 0 && numTwo.CompareTo(numThree) > 0)
+            {
+                return numTwo;
+            }
+            else if (numThree.CompareTo(numTwo) > 0 && numThree.CompareTo(numOne) > 0)
+            {
+                return numThree;
+            }
+            else
+            {
+                Console.WriteLine("All number are same ");
+                return 0;
+            }
+        }
+    }
+}
--- MaximumNum/MaximumNum/Program.cs
+++ MaximumNum/MaximumNum/Program.cs
@@ -4,29 +4,6 @@ using System.Text;
 
 namespace MaximumNum
 {
-    class MaxNum
-    {
-        public int Maximum(int numOne, int numTwo, int numThree)
-        {
-            if (numOne.CompareTo(numTwo) > 0 && numOne.CompareTo(numThree) > 0)
-            {
-                return numOne;
-            }
-            else if (numTwo.CompareTo(numOne) > 0 && numTwo.CompareTo(numThree) > 0)
-            {
-                return numTwo;
-            }
-            else if (numThree.CompareTo(numTwo) > 0 && numThree.CompareTo(numOne) > 0)
-            {
-                return numThree;
-            }
-            else
-            {
-                Console.WriteLine("All number are same ");
-                return 0;
-            }
-        }
-    }
     class Program
     {
        public static void Main(string[] args)
@@ -34,6 +11,10 @@ namespace MaximumNum
             MaxNum maxNum = new MaxNum();
             maxNum.Maximum(10, 25, 53);
             Console.WriteLine("The Maximum Number is: " + maxNum.Maximum(10, 25, 53));
+
+            FloatMaxNum floatMaxNum = new FloatMaxNum();
+            floatMaxNum.MaxfloatNum(77.6f, 88.4f, 56.4f);
+            Console.WriteLine("The Maximum Float Number is: " + floatMaxNum.MaxfloatNum(77.6f, 88.4f, 56.4f));
        } 
     }
 }
