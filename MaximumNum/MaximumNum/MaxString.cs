997eb23c4eab268126fd3cc9bbbd16a3e040882f
yburkul <yburkul123@gmail.com>
2022-03-27 13:06:58 +0530

    Added:Maximum String

---
+++ MaximumNum/MaximumNum/MaxString.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximumNum
+{
+    class MaxString
+    {
+        public string MaximumString(string strOne, string strTwo, string strThree)
+        {
+           if(strOne.CompareTo(strTwo) > 0 && strOne.CompareTo(strThree) > 0)
+           {
+                return strOne;
+           }
+           else if (strTwo.CompareTo(strOne) > 0 && strTwo.CompareTo(strThree) > 0)
+           {
+                return strTwo;
+           }
+           else if (strThree.CompareTo(strTwo) > 0 && strThree.CompareTo(strOne) < 0)
+           {
+                return strThree;
+           }
+           return null;
+        }
+    }
+}
--- MaximumNum/MaximumNum/Program.cs
+++ MaximumNum/MaximumNum/Program.cs
@@ -15,6 +15,10 @@ namespace MaximumNum
             FloatMaxNum floatMaxNum = new FloatMaxNum();
             floatMaxNum.MaxfloatNum(77.6f, 88.4f, 56.4f);
             Console.WriteLine("The Maximum Float Number is: " + floatMaxNum.MaxfloatNum(77.6f, 88.4f, 56.4f));
+
+            MaxString maxString = new MaxString();
+            maxString.MaximumString("Apple", "Banana", "Peach");
+            Console.WriteLine("The Maximum String is: " + maxString.MaximumString("Apple", "Peach", "Banana"));
        } 
     }
 }
